95f59a08a8dffa65795869f8e0be37d7c7bedd74
Hohichh <verkovich2005@gmail.com>
2025-10-22 14:35:14 +0300

    change project structure for src/main and src/test to io.hohichh domain

--- src/main/java/io/hohichh/marketplace/DemoApplication.java
+++ src/main/java/io/hohichh/marketplace/DemoApplication.java
@@ -1,20 +1,10 @@
-package com.example.demo;
+package io.hohichh.marketplace;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-
-//test
-	@GetMapping("/")
-	public String home() {
-		return "Spring is here!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/test/java/io/hohichh/marketplace/DemoApplicationTests.java
+++ src/test/java/io/hohichh/marketplace/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package io.hohichh.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
