61d92e9f68db7a21dd64481f7907f33189825c5c
Hohichh <verkovich2005@gmail.com>
2025-10-24 18:19:08 +0300

    фÐadd config files

--- src/test/java/io/hohichh/marketplace/DemoApplicationTests.java
+++ src/test/java/io/hohichh/marketplace/DemoApplicationTests.java
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
